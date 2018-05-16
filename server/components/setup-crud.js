1116c96b4c52a9d77a2f09901873d7dcae1f7f8f
Jazon Lubaton <jahskee@yahoo.com>
2018-05-16 19:38:17 -0400

    update

--- server/components/setup-crud.js
+++ server/components/setup-crud.js
@@ -2,10 +2,10 @@
 var RouterFactory = require('node-express-crud-router').RouterFactory;
 
 setupCrud = (app) => {
-    var customerRouter = RouterFactory.create({path: "customers",model: require('./models/customerModel')});
+    var customerRouter = RouterFactory.create({path: "customers",model: require('./models/Customer')});
     app.use("/api", customerRouter);
 
-    var contactRouter = RouterFactory.create({path: "contacts",model: require('./models/contactModel')});
+    var contactRouter = RouterFactory.create({path: "contacts",model: require('./models/Contact')});
     app.use("/api", contactRouter);
 }
 
